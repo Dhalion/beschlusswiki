c5890035f48f2e8076b13b1d669acea251827099
Christopher <christopher.falkner@web.de>
2023-10-08 19:10:16 +0200

    fix: [frontend] changed ResolutionView to new schema

--- beschlusswiki-frontend/components/resolution/ResolutionView.vue
+++ beschlusswiki-frontend/components/resolution/ResolutionView.vue
@@ -1,21 +1,38 @@
 
 <template>
     <div class="flex justify-center text-black">
-
-        <div class="flex flex-col h-full w-7/12 ring-1 ring-gray-200 shadow">
-            <ResolutionViewResolutionHead :resolution="resolution" />
-            <ResolutionViewResolutionBody :resolution="resolution.fließtext" :error="error" />
+        <div v-if="resolution">
+            <div class="flex flex-col h-full w-7/12 ring-1 ring-gray-200 shadow">
+                <ResolutionViewResolutionHead :resolution="resolution" />
+                <ResolutionViewResolutionBody :resolution="resolution.body.text" :error="error" />
+            </div>
         </div>
-
+        <div v-if="!resolution && !error">
+            Loading
+        </div>
+        <span v-if="error">Error: {{ error }}</span>
     </div>
 </template>
 
 <script setup>
 const runtimeConfig = useRuntimeConfig();
-const API_ENDPOINT = "http://orca-test:1313"
+const API_ENDPOINT = runtimeConfig.public.apiEndpoint;
 // Get resolution ID from Route Url Parameters
 const route = useRoute();
 const resolutionId = route.params.id;
-const { data: resolution, pending, error, refresh } = await useFetch(API_ENDPOINT + '/api/antrag/' + resolutionId);
+
+const resolution = computed(() => {
+    return data.value;
+});
+
+
+
+const { data, pending, error, refresh } =
+    await useFetch("/resolution", {
+        baseURL: API_ENDPOINT,
+        params: {
+            id: resolutionId
+        },
+    });
 
 </script>
--- beschlusswiki-frontend/components/resolution/ResolutionViewResolutionHead.vue
+++ beschlusswiki-frontend/components/resolution/ResolutionViewResolutionHead.vue
@@ -2,19 +2,19 @@
     <div class="bg-hellrosa text-beere flex justify-center 
         justify-self-center place-items-center">
         <div class="flex flex-col items-center p-10">
-            <span class="font-black text-8xl pb-4">{{ resolution.tag }}</span>
-            <span class="font-black text-6xl">{{ resolution.jahr }}</span>
+            <span class="font-black text-8xl pb-4">{{ resolution.body.tag }}</span>
+            <span class="font-black text-6xl">{{ resolution.body.year }}</span>
         </div>
         <div class="w-3/4 p-1 font-bold text-4xl 
         line-clamp-3 text-clip pr-4">
-            {{ resolution.titel }}
+            {{ resolution.body.title }}
         </div>
     </div>
     <!-- Category and Origin Bar -->
     <div class="bg-beere flex justify-between items-center p-1 px-3">
         <span class="text-xs font-sans text-white antialiased"> Kategorie / Unterkategorie </span>
         <span class="px-3 my-1 py-1 bg-hellrosa text-beere rounded-full text-sm tracking-wide"
-            v-for="applicant in resolution.antragsteller">Bayern</span>
+            v-for="applicant in resolution.body.applicants">Bayern</span>
     </div>
 </template>
 
@@ -23,9 +23,12 @@ const { resolution } = defineProps({
     resolution: Object,
 })
 
+onMounted(() => {
+    console.log(resolution);
+})
 
 const applicants = () => {
-    resolution.antragsteller.forEach((applicant) => {
+    resolution.body.applicants.forEach((applicant) => {
         return applicant;
     })
 };
