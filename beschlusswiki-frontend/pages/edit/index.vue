99972fb731c000e37b645ea047c462f5a3af13fe
Christopher <christopher.falkner@web.de>
2023-10-22 16:16:30 +0200

    style(ui): Edit page responsive style for small devices

--- beschlusswiki-frontend/pages/edit/index.vue
+++ beschlusswiki-frontend/pages/edit/index.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="w-screen">
 
-        <EditResolutionHead class="w-5/6 mx-auto" v-if="!isLoading" />
+        <EditResolutionHead class="md:w-5/6 mx-auto" v-if="!isLoading" />
         <div v-if="plainMarkdown">
-            <div class="w-5/6 mx-auto flex mt-2 justify-center">
-                <span class="text-gray-400 text-lg w-1/2 text-center">Beschluss bearbeiten </span>
-                <span class="text-gray-400 text-lg w-1/2 text-center">Vorschau</span>
+            <div class="md:w-5/6 mx-auto flex mt-2 justify-center">
+                <span class="text-gray-400 md:text-lg text-sm w-1/2 text-center">Beschluss bearbeiten </span>
+                <span class="text-gray-400 md:text-lg text-sm w-1/2 text-center">Vorschau</span>
             </div>
-            <div class="text-black markdown mx-auto w-5/6 flex justify-center divide-x pt-3">
+            <div class="text-black markdown mx-auto md:w-5/6 flex justify-center divide-x pt-3">
                 <div class="w-1/2 h-screen p-3">
-                    <UTextarea v-model="plainMarkdown" variant="none" autoresize
-                        class="font-mono text-base outline outline-1 outline-gray-300 outline-offset-1 bg-white" />
+                    <UTextarea v-model="plainMarkdown" variant="none" autoresize size="xs"
+                        class="font-mono md:text-base text-xs outline outline-1 outline-gray-300 outline-offset-1 bg-white" />
                 </div>
-                <div class="w-1/2 p-3">
-                    <div v-html="parsedMarkdown" class="prose w-full"></div>
+                <div class="w-1/2 md:p-3 p-1">
+                    <div v-html="parsedMarkdown" class="prose w-full text-sm pr-3"></div>
                     <!-- {{ plainMarkdown }} -->
                 </div>
             </div>
