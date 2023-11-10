b51ba5d957b36261f432f97d0f9d476f15bdee81
Christopher <christopher.falkner@web.de>
2023-11-10 22:00:41 +0100

    style: fixed category cards same size

--- beschlusswiki-frontend/components/main/MainCategoriesView.vue
+++ beschlusswiki-frontend/components/main/MainCategoriesView.vue
@@ -1,108 +1,28 @@
 <template>
   <div class="flex flex-col items-center mt-10">
+    <SharedLoadingSpinner v-if="pending" text="Loading Categories" />
+
     <!-- Category Cards Container -->
-    <div
-      class="bg-white h-full text-black w-3/4 items-center grid gap-4 grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
-    >
+    <div class="bg-white h-full text-black mx-5 sm:w-3/4 items-center grid gap-6 grid-cols-2
+                  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       <!-- Category Cards -->
-      <MainCategoryCard
-        v-for="category in categories"
-        :key="category.id"
-        :category-title="category.title"
-        :category-image="category.image"
-      />
+      <div v-for="category in categories" :key="category._id" class="flex flex-col h-full">
+        <MainCategoryCard :title="category.name" :id="category._id" :tag="category.tag"
+          :resolutions="category.resolutions" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 const search = useSearch();
+const config = useRuntimeConfig();
 
-const categories = [
-  {
-    title: "Bildung",
-    image: "Bildung.svg",
-    id: 1,
-  },
-  {
-    title: "Europa und Internationales",
-    image: "EuropaUndInternationales.svg",
-    id: 2,
-  },
-  {
-    title: "Gegen Rechts",
-    image: "GegenRechts.svg",
-    id: 3,
-  },
-  {
-    title: "Arbeitsprogramme",
-    image: "Arbeitsprogramme.svg",
-    id: 4,
-  },
-  {
-    title: "Digitales",
-    image: "Digitales.svg",
-    id: 5,
-  },
-  {
-    title: "Einwanderung",
-    image: "Einwanderung.svg",
-    id: 6,
-  },
-  {
-    title: "Familie",
-    image: "Familie.svg",
-    id: 7,
-  },
-  {
-    title: "Feminismus",
-    image: "Feminismus.svg",
-    id: 8,
-  },
-  {
-    title: "Gesundheit",
-    image: "Gesundheit.svg",
-    id: 9,
-  },
-  {
-    title: "Justiz und Inneres",
-    image: "JustizUndInneres.svg",
-    id: 10,
-  },
-  {
-    title: "Kommunales und Landespolitik",
-    image: "KommunalesUndLandespolitik.svg",
-    id: 11,
-  },
-  {
-    title: "LGBTIQ",
-    image: "LGBTIQ.svg",
-    id: 12,
-  },
-  {
-    title: "Partei und Jusos",
-    image: "ParteiUndJusos.svg",
-    id: 13,
-  },
-  {
-    title: "Satzungsänderungen",
-    image: "Satzungsänderungen.svg",
-    id: 14,
-  },
-  {
-    title: "Umwelt und Energie",
-    image: "UmweltUndEnergie.svg",
-    id: 15,
-  },
-  {
-    title: "Verkehr und Infrastruktur",
-    image: "VerkehrUndInfrastruktur.svg",
-    id: 16,
-  },
-  {
-    title: "Wirtschaft und Soziales",
-    image: "WirtschaftUndSoziales.svg",
-    id: 17,
-  },
-];
+// const cardsContainerWidth = ref(window.innerWidth * .75);
+
+
+const { data: categories, error, pending, refresh } = useLazyFetch("/category", {
+  baseURL: config.public.apiEndpoint,
+  transform: (data) => data.categories,
+});
 </script>
---
+++ beschlusswiki-frontend/components/shared/loadingSpinner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] text-jusorot-600"
+    role="status"></div>
+  <span class="text-slate-500 pt-4"> {{ props.text }} </span>
+</template>
+
+<script lang="ts" setup>
+
+// Define "title" prop
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+</script>
+
+
