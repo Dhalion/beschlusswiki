f5a469b69ac5d7c65faecaadade386668ada5cdf
Christopher <christopher.falkner@web.de>
2024-02-15 21:09:45 +0100

    fix: excessive stack depth in request resolved

--- app/pages/edit/index.vue
+++ app/pages/edit/index.vue
@@ -187,7 +187,7 @@ async function submit() {
     return;
   }
 
-  const response: Response = await $fetch("/resolution", {
+  const res = await $fetch("/api/resolution", {
     baseURL: config.public.apiEndpoint,
     method: "PUT",
     headers: {
@@ -200,7 +200,7 @@ async function submit() {
     body: { resolution: resolution.value },
   });
 
-
+  const response = res as Response;
 
   if (response.status !== 200) {
     postError.value = response.status + " - " + response.statusText;
