655e86c822a9abca8420831ad68aca73b322ddd1
Christopher <christopher.falkner@web.de>
2024-02-22 13:48:18 +0100

    feat: header clickable

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -9,7 +9,7 @@
           @click="handleLogoClick" />
       </NuxtLink>
       <!-- Navbar Heading "Jusos Beschlusswiki" centered, semi-bold -->
-      <NuxtLink href="#" class="xl:text-2xl text-xl font-extrabold flex-nowrap text-white">
+      <NuxtLink href="/" @click="handleLogoClick" class="xl:text-2xl text-xl font-extrabold flex-nowrap text-white">
         Jusos Beschlusswiki
       </NuxtLink>
     </div>
