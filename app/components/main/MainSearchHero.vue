38663ed23f247f2f5cd3e90f7fc55d61373bdfc3
Christopher <christopher.falkner@web.de>
2024-03-10 14:53:51 +0100

    feat: year and applicants in advancedSearch

--- app/components/main/MainSearchHero.vue
+++ app/components/main/MainSearchHero.vue
@@ -12,19 +12,46 @@
         <UAccordion color="cool" variant="link" :items="advancedSearchItems" class="mb-5">
           <template #advanced-search>
             <div v-if="!categoriesPending && !categoriesError">
-              <USelectMenu v-model="advancedSearch.categories" v-if="categoriesData" :options="categoriesData" multiple
-                searchable>
-                <template #label>
-                  <span v-if="advancedSearch.categories.length" class="truncate">
-                    {{ advancedSearch.categories.map((category) => `${category.tag} - ${category.name}`).join(", ") }}
-                  </span>
-                  <span v-else>Kategorie(n) wählen</span>
-                </template>
+              <UFormGroup label="Kategorie(n)">
+                <USelectMenu v-model="advancedSearch.categories" v-if="categoriesData" :options="categoriesData"
+                  multiple searchable>
+                  <template #label>
+                    <span v-if="advancedSearch.categories.length" class="truncate">
+                      {{ advancedSearch.categories.map((category) => `${category.tag} - ${category.name}`).join(", ") }}
+                    </span>
+                    <span v-else>Kategorie(n) wählen</span>
+                  </template>
 
-                <template #option="{ option }">
-                  <span>{{ option.tag }} - {{ option.name }}</span>
-                </template>
-              </USelectMenu>
+                  <template #option="{ option }">
+                    <span>{{ option.tag }} - {{ option.name }}</span>
+                  </template>
+                </USelectMenu>
+              </UFormGroup>
+
+              <UFormGroup label="Zeitraum">
+                <div class="flex flex-row gap-3">
+                  <span class="text-center self-center">Von</span>
+                  <UInput type="number" v-model="advancedSearch.fromYear" />
+                  <span class="text-center self-center">Bis</span>
+                  <UInput type="number" v-model="advancedSearch.toYear" />
+                </div>
+              </UFormGroup>
+
+              <UFormGroup label="Antragsteller*innen">
+                <USelectMenu v-model="advancedSearch.applicants" v-if="applicantsData" :options="applicantsData"
+                  multiple searchable>
+                  <template #label>
+                    <span v-if="advancedSearch.applicants.length" class="truncate">
+                      {{ advancedSearch.applicants.map((applicant) => applicant.name).join(", ") }}
+                    </span>
+                    <span v-else>Antragsteller*innen wählen</span>
+                  </template>
+
+                  <template #option="{ option }">
+                    <span>{{ option.name }}</span>
+                  </template>
+                </USelectMenu>
+              </UFormGroup>
             </div>
             <div v-else-if="categoriesPending">Loading...</div>
           </template>
@@ -36,6 +63,7 @@
 
 <script setup lang="ts">
 import type { ICategory } from '~/types/Interfaces';
+import type { IApplicant } from '~/types/models/applicant.schema';
 
 
 
@@ -45,6 +73,9 @@ const emit = defineEmits(["update:modelValue"]);
 const searchInput = ref(props.modelValue || "");
 const advancedSearch = ref({
   categories: <ICategory[]>[],
+  applicants: <IApplicant[]>[],
+  fromYear: undefined,
+  toYear: undefined,
 });
 
 const handleSearchInput = (event: Event) => {
@@ -56,6 +87,12 @@ const { data: categoriesData,
   error: categoriesError,
   refresh: categoriesRefresh
 } = await useFetch<ICategory[]>('/api/category');
+
+const { data: applicantsData,
+  pending: applicantsPending,
+  error: applicantsError,
+  refresh: applicantsRefresh
+} = await useFetch<IApplicant[]>('/api/applicants');
 
 const advancedSearchItems = [{
   label: "Erweiterte Suche",
